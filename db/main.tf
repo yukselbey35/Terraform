67cb756fe5986d5a4c02239af8b3590f35f3f6d0
yukselbey35 <yukselbey35@gmail.com>
2022-04-01 08:05:33 -0700

    ami, igw, route table, etc added on main tf

--- db/main.tf
+++ db/main.tf
@@ -81,19 +81,29 @@ resource "aws_key_pair" "mtc_auth" {
 
 
 resource "aws_instance" "dev_node" {
-  instance_type = "t2.micro"
-  ami           = data.aws_ami.server_ami.id
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+
+  #Add user data
+  user_data = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 8
+  }
 
   tags = {
     Name = "dev_node"
   }
 
-  key_name               = aws_key_pair.mtc_auth.id
-  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-  subnet_id              = aws_subnet.mtc_public_subnet.id
-
-  root_block_device {
-    volume_size = 8
+  provisioner "local-exec" {
+    command = templatefile("windows-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user     = "ubuntu",
+    identityfile = "~/.ssh/mtckey" })
+    interpreter = ["Powershell", "-Command"]
   }
 }
 
