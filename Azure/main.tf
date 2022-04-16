0fa2083b988ade8f14e3faaab70848ed54f183f0
yukselbey35 <yukselbey35@gmail.com>
2022-04-16 09:05:05 -0700

    vn, subnet, sg, sg a added to main tf

--- Azure/main.tf
+++ Azure/main.tf
@@ -25,21 +25,42 @@ resource "azurerm_virtual_network" "tc-vn" {
   location            = azurerm_resource_group.tc-rg.location
   resource_group_name = azurerm_resource_group.tc-rg.name
   address_space       = ["10.0.0.0/16"]
-  /*  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-*/
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_subnet" "tc-subnet" {
+  name                 = "tc-subnet"
+  resource_group_name  = azurerm_resource_group.tc-rg.name
+  virtual_network_name = azurerm_virtual_network.tc-vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+resource "azurerm_network_security_group" "tc-sg" {
+  name                = "tc-sg"
+  location            = azurerm_resource_group.tc-rg.location
+  resource_group_name = azurerm_resource_group.tc-rg.name
+
+  security_rule {
+    name                       = "tc-dev-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "tc-sga" {
+  subnet_id                 = azurerm_subnet.tc-subnet.id
+  network_security_group_id = azurerm_network_security_group.tc-sg.id
 }
