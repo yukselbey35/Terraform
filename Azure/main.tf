dcfdf5fad0490a227e285efffa4690831397bc13
yukselbey35 <yukselbey35@gmail.com>
2022-04-16 07:56:24 -0700

    main.tf created, providers and a resource group added

--- Azure/main.tf
+++ Azure/main.tf
@@ -19,3 +19,27 @@ resource "azurerm_resource_group" "tc-rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "tc-vn" {
+  name                = "tc-network"
+  location            = azurerm_resource_group.tc-rg.location
+  resource_group_name = azurerm_resource_group.tc-rg.name
+  address_space       = ["10.0.0.0/16"]
+  /*  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+*/
+  tags = {
+    environment = "dev"
+  }
+}
