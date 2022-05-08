e0da77078b6097afc9317edb0cef6e44affd9933
yukselbey35 <yukselbey35@gmail.com>
2022-05-08 13:37:27 -0700

    Example Creating VPC with variable.tf and tfvars

---
+++ AWS/main.tf
@@ -0,0 +1,68 @@
+# Create the VPC
+resource "aws_vpc" "feature_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+#Create the internetgateway
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.feature_vpc.id
+
+  tags = {
+    Name = var.IGW_name
+  }
+}
+
+#Create public subnet
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id     = aws_vpc.feature_vpc.id
+  cidr_block = var.cidr_block_public_subnet_1a
+  availability_zone = var.availability_zone_public_subnet_1a
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.public_subnet_1a
+  }
+}
+
+#Create public subnet
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id     = aws_vpc.feature_vpc.id
+  cidr_block = var.cidr_block_public_subnet_1b
+  availability_zone = var.availability_zone_public_subnet_1b
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.public_subnet_1b
+  }
+}
+
+#Create private subnet
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id     = aws_vpc.feature_vpc.id
+  cidr_block = var.cidr_block_private_subnet_1a
+  availability_zone = var.availability_zone_private_subnet_1a
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = var.private_subnet_1a
+  }
+}
+
+#Create private subnet 
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id     = aws_vpc.feature_vpc.id
+  cidr_block = var.cidr_block_private_subnet_1b
+  availability_zone = var.availability_zone_private_subnet_1b
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = var.private_subnet_1b
+  }
+}
---
+++ AWS/terraform.tfvars
@@ -0,0 +1,2 @@
+vpc_name = "dev_VPC"
+IGW_name = "feature_vpc_InternetGateway"
